020e86b1f1a11a0a8affd5ff20420d8af4bcf85c
VenomousJ <43055670+VenomousJ@users.noreply.github.com>
2021-06-29 23:58:58 +0530

    Update services.js

--- src/Components/services.js
+++ src/Components/services.js
@@ -4,7 +4,7 @@ class services
 {
     async getSimilarWords(word)
     {
-        const URL = "https://words.bighugelabs.com/api/2/19361a98e80fba07832c0fec3aa0a8e2/"+word+"/json";
+        const URL = "https://words.bighugelabs.com/api/2/{YOUR_API_KEY_HERE}/"+word+"/json";
         return await axios.get(URL)
     }
 
